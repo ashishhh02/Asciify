ec9ec6f39ccecca28f53c10ee6edcd97c688c88f
ashishhh02 <97935756+ashishhh02@users.noreply.github.com>
2022-06-21 23:36:01 +0530

    Add files via upload

---
+++ video_to_frames.py
@@ -0,0 +1,27 @@
+import cv2
+import os
+
+cam = cv2.VideoCapture(r"C:\Users\Fabian\Desktop\Video to ascii project\yt1s.com - van darkholme intro.mp4")
+try:
+    if not os.path.exists('data'):
+        os.makedirs('data')
+except OSError:
+    print('Error: Creating directory of data')
+
+current_frame = 0
+
+while(True):
+    ret,frame = cam.read()
+    
+    if ret:
+        name = './data/frame' + str(current_frame) + '.jpg'
+        print('Creating...' + name)
+
+        #writing the extracted images
+        cv2.imwrite(name, frame)
+
+        current_frame += 1
+    else:
+        break
+cam.release()
+cv2.destroyAllWindows()
